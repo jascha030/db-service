91aa3fd8c24a3c1033d5e0603b515e02c2ad88b8
Jascha030 <contact@jaschavanaalst.nl>
2023-02-19 23:58:37 +0100

    🚧 Create test-output dir during tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,8 +4,25 @@ declare(strict_types=1);
 
 use Dotenv\Dotenv;
 
-(static function () {
+(static function (): void {
     require dirname(__DIR__) . '/vendor/autoload.php';
 
     Dotenv::createImmutable(__DIR__)->load();
+
+    if (is_dir($testDir = dirname(__DIR__) . '/test-output')) {
+        $iterator = new FilesystemIterator($testDir);
+
+        /** @var SplFileInfo $file */
+        foreach ($iterator as $file) {
+            @unlink($file->getRealPath());
+        }
+
+        rmdir($testDir);
+    }
+
+    if (! mkdir($testDir) && ! is_dir($testDir)) {
+        throw new RuntimeException(sprintf('Directory "%s" was not created', $testDir));
+    }
+
+    define('TEST_OUTPUT_DIR', $testDir);
 })();

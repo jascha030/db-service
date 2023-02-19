d4340dba0ee0ce4d6a631ba9a080be0723eb10d6
Jascha030 <contact@jaschavanaalst.nl>
2023-02-19 23:58:50 +0100

    ✅ Add tests for MySQLDumper.

--- tests/Database/Dumper/MysqlDumperTest.php
+++ tests/Database/Dumper/MysqlDumperTest.php
@@ -1,10 +1,52 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\DB\Database\Dumper;
 
+use Doctrine\DBAL\Exception\InvalidArgumentException;
+use Jascha030\DB\Exception\SystemDependencyException;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertFileDoesNotExist;
+use function PHPUnit\Framework\assertFileExists;
 
+/**
+ * @covers \Jascha030\DB\Database\Dumper\DumperAbstract
+ * @covers \Jascha030\DB\Database\Dumper\MysqlDumper
+ */
 class MysqlDumperTest extends TestCase
 {
+    /**
+     * @throws InvalidArgumentException
+     * @throws SystemDependencyException
+     */
+    public function testDump(): void
+    {
+        if (! isset($_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DUMPER_TEST_DATABASE'])) {
+            static::fail('Could not find required credentials in environment.');
+        }
 
+        $dumper   = new MysqlDumper();
+        $expected = TEST_OUTPUT_DIR . '/' . $_ENV['DUMPER_TEST_DATABASE'] . '.sql';
+
+        assertFileDoesNotExist($expected);
+
+        assertEquals(
+            Command::SUCCESS,
+            $dumper->dump(
+                [
+                    'user'     => $_ENV['DB_USER'],
+                    'password' => $_ENV['DB_PASSWORD'],
+                    'dbname'   => $_ENV['DUMPER_TEST_DATABASE'],
+                ],
+                TEST_OUTPUT_DIR,
+                new ConsoleOutput()
+            )
+        );
+
+        assertFileExists($expected);
+    }
 }
